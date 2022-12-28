e69b2f51ea37e0f5a600d2b4f9903b4ea74df9d4
kennedy-ak <akogokennedy@gmail.com>
2022-12-28 00:52:27 +0000

    adding blogs to the database

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,7 @@ from .models import Post, Author, Tag
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     list_filter = ("author", "tag","date")
+    list_display = ("title","author","date")
     prepopulated_fields = {"slug":("title",) }
     
 
