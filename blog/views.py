c4639a58c5247ee3027391bc68a380d0ec68715d
kennedy-ak <akogokennedy@gmail.com>
2023-01-24 05:53:15 +0000

    made changes to certain aspect of the blog

--- blog/views.py
+++ blog/views.py
@@ -24,5 +24,6 @@ def all_post(request):
 def single_post_page(request, slug):
     identified_post =get_object_or_404(Post,slug = slug)
     return render(request, "blog/post-detail.html ",{
-        "post":identified_post
+        "post":identified_post,
+        "post_tags":identified_post.tag.all()
     })
