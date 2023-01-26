32a93d5ac7969843fd94810a1b6f6774edae73d9
kennedy-ak <akogokennedy@gmail.com>
2023-01-26 20:01:49 +0000

    adding a comment form, comment model and styling it

--- blog/models.py
+++ blog/models.py
@@ -32,6 +32,11 @@ class Post(models.Model):
     
     
     
+class Comment(models.Model):
+    user_name = models.CharField(max_length=20)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=300)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE , related_name="comments")
     
 
     
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render ,get_object_or_404
 from .models import Post
+from .forms import CommentForm
 from django.views.generic import ListView, DetailView
+
 
 
 class StartPageView(ListView):
@@ -44,6 +46,7 @@ class SinglePostView(DetailView):
     def get_context_data(self, **kwargs):
         context= super().get_context_data(**kwargs)
         context["post_tags"] = self.object.tag.all()
+        context["comment_form"] = CommentForm()
         return context
     
     
