241eef8dcf2b66ff32edfa6e5cf993fc549f3978
kennedy-ak <akogokennedy@gmail.com>
2023-01-28 14:07:20 +0000

    showing the comment to the user under the post

--- blog/models.py
+++ blog/models.py
@@ -30,13 +30,16 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     content = models.TextField(validators=[MinLengthValidator(10)],blank=True)
     
-    
+    def __str__(self):
+        return f"{self.title}"
     
 class Comment(models.Model):
     user_name = models.CharField(max_length=20)
     user_email = models.EmailField()
     text = models.TextField(max_length=300)
     post = models.ForeignKey(Post, on_delete=models.CASCADE , related_name="comments")
+    
+    
     
 
     
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render ,get_object_or_404
 from .models import Post
 from .forms import CommentForm
 from django.views.generic import ListView, DetailView
-
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 class StartPageView(ListView):
@@ -38,16 +40,44 @@ class AllPostView(ListView):
     context_object_name ="all_posts"
     
     
-class SinglePostView(DetailView):
-    template_name ="blog/post-detail.html"
-    model = Post
+class SinglePostView(View):
+    # template_name ="blog/post-detail.html"
+    # model = Post
     
-    
-    def get_context_data(self, **kwargs):
-        context= super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tag.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def get(self,request, slug):
+        post = Post.objects.get(slug = slug)
+        context ={
+            "post":post,
+            "post_tag":post.tag.all(),
+            "comment_form": CommentForm,
+            "comments":post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("single_post",args=[slug]))
+
+        context ={
+            "post":post,
+            "post_tag":post.tag.all(),
+            "comment_form": CommentForm,
+            "comments":post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-detail.html", context)        
+        
+    # def get_context_data(self, **kwargs):
+    #     context= super().get_context_data(**kwargs)
+    #     context["post_tags"] = self.object.tag.all()
+    #     context["comment_form"] = CommentForm()
+    #     return context
     
     
 
